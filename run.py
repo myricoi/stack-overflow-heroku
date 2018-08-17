9ff06f915c5c0bded353558118d247ca12d23511
myricoi <myricoidixon@gmail.com>
2018-08-17 12:03:35 +0300

    add pep8 conventions [159823946]

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask
 from StackOverflow_Lite.api.version_1.views import routes
 app = Flask(__name__)
 
-app.register_blueprint(routes.mod, url_prefix = '/api/v1')
+app.register_blueprint(routes.mod, url_prefix='/api/v1')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
