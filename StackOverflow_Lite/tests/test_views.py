6c9ae1b624c365b6f54ba1523b89edd74c9f4a2a
myricoi <myricoidixon@gmail.com>
2018-08-20 02:39:37 +0300

    deactivate debug mode

--- StackOverflow_Lite/tests/test_views.py
+++ StackOverflow_Lite/tests/test_views.py
@@ -1,18 +1,2 @@
 from flask import Flask, jsonify, make_response
 from ..api.version_1.views import routes
-
-app = Flask(__name__)
-app.register_blueprint(routes.mod)
-
-BASE_URL = 'http://127.0.0.1:5000/api/v1'
-
-
-def test_get_all_questions():
-    tester_app = app
-    tester_ctx = tester_app.app_context()
-    tester_ctx.push()
-    tester_client = tester_app.test_client()
-    response = tester_client.get(BASE_URL + '/questions')
-    expected = make_response(jsonify({}))
-    assert response == expected
-    tester_ctx.pop()
--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 from flask import Flask
 
+
 app = Flask(__name__)
-
-from StackOverflow_Lite.api.version_1.views import routes
-from StackOverflow_Lite.tests import test_views
-
-app.register_blueprint(routes.mod, url_prefix='/api/v1')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    from StackOverflow_Lite.api.version_1.views import routes
+    from StackOverflow_Lite.tests import test_views
+    app.register_blueprint(routes.mod, url_prefix='/api/v1')
+    app.run()
